8d2a84a25f623aff40bc4443e078d9c173ee6f9a
Casey <c.proctor527@gmail.com>
2021-05-26 17:04:50 +0800

    tttt

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <?php
 global $more;//define a global variable
 $more = 0;// the global varibale is now equal to 0
-query_posts('cat=2');//look for posts that have the category of 2
+query_posts('cat=4');//look for posts that have the category of 2
 if(have_posts()) ://if we have posts to display
 while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
 ?>
@@ -29,22 +29,29 @@ wp_reset_query();?>
                     <h3 class="text-center">DISCOVER THE BATAVIA</h3>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/sub1/wp-content/themes/starter_for_josh_site/images/game.jpg ">
-                    <h4>Barbarism &amp; Brutality</h4>
-                    <p>On the 28 October the VOC (Dutch East India Company) ship <i>Batavia</i> left Texel in The Netherlands bound for Batavia, the company's headquarters in the East.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 128;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>                       
+
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/sub1/wp-content/themes/starter_for_josh_site/images/game.jpg ">
-                    <h4>The Portico Fresco</h4>
-                    <p>During the 1972-76 excavation of <i>Batavia</i>, divers raised 137 shaped sandstone building blocks weighing a total of 37 tonnes. Like a giant jigsaw, they fitted together to form a magnificent portico façade.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 131;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?>  
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/sub1/wp-content/themes/starter_for_josh_site/images/game.jpg ">
-                    <h4>Lost Riches</h4>
-                    <p>During the 1972-76 excavation of <i>Batavia</i>, divers raised 137 shaped sandstone building blocks weighing a total of 37 tonnes. Like a giant jigsaw, they fitted together to form a magnificent portico façade.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 133;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="readmore" href="'.get_permalink($queried_post).'"><br>CONTINUE READING</a>';?> 
                 </div>
             </div>
         </div>
