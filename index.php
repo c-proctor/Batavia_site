41ad463d5fe6b767811c03f6af56a3085a16c86e
c-proctor <79915136+c-proctor@users.noreply.github.com>
2021-06-02 11:30:43 +0800

    php posts

--- index.php
+++ index.php
@@ -4,8 +4,19 @@
         <div class="container about-lg">
             <div class="row">
                 <div class="col-md-6 aboutdiv">
-                    <h2 class="text-center"> A shipwreck turns to tragedy </h2>
-                    <p class="about-text"> Almost 400 years ago, in the hours before dawn on 4 June 1629, a flagship of the Dutch East India Company (VOC) was wrecked upon Morning Reef near Beacon Island, some 60 kilometres off the Western Australian coast. <br> <br> It was the maiden voyage of the <i>Batavia</i>, bound for the Dutch East Indian colonies of modern-day Jakarta, but the tragedy of shipwreck would be overshadowed by the subsequent mutiny among the survivors on the isolated Houtman Abrolhos Islands.</p>
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=4');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>
                 </div>
                 <div class="col-md-6">
                 </div>
@@ -15,8 +26,19 @@
         <!--displays at 425px screen size-->
         <div class="container about-sm">
             <div class="aboutdiv">
-                <h2 class="text-center"> A shipwreck turns to tragedy </h2>
-                <p class="about-text"> Almost 400 years ago, in the hours before dawn on 4 June 1629, a flagship of the Dutch East India Company (VOC) was wrecked upon Morning Reef near Beacon Island, some 60 kilometres off the Western Australian coast. <br> <br> It was the maiden voyage of the <i>Batavia</i>, bound for the Dutch East Indian colonies of modern-day Jakarta, but the tragedy of shipwreck would be overshadowed by the subsequent mutiny among the survivors on the isolated Houtman Abrolhos Islands.</p>
+<?php
+global $more;//define a global variable
+$more = 0;// the global varibale is now equal to 0
+query_posts('cat=4');//look for posts that have the category of 2
+if(have_posts()) ://if we have posts to display
+while(have_posts()) :the_post();//LOOP through all the posts and find the one that has a category of 2 get thet title and content
+?>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2> 
+            <div><p class="about-text"><?php the_content() ?></p></div>
+<?php
+endwhile;
+endif;
+wp_reset_query();?>
             </div>
         </div>
     </section>
@@ -28,22 +50,28 @@
                     <h3 class="text-center">DISCOVER THE BATAVIA</h3>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/wp-content/themes/batavia_site/images/skeleton.jpg">
-                    <h4>Barbarism &amp; Brutality</h4>
-                    <p>On the 28 October the VOC (Dutch East India Company) ship <i>Batavia</i> left Texel in The Netherlands bound for Batavia, the company's headquarters in the East.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 128;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="continue" href="'.get_permalink($queried_post).'"><br>Continue Reading</a>';?>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/wp-content/themes/batavia_site/images/fresco.jpg">
-                    <h4>The Portico Fresco</h4>
-                    <p>During the 1972-76 excavation of <i>Batavia</i>, divers raised 137 shaped sandstone building blocks weighing a total of 37 tonnes. Like a giant jigsaw, they fitted together to form a magnificent portico façade.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 131;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="continue" href="'.get_permalink($queried_post).'"><br>Continue Reading</a>';?>
                 </div>
                 <div class="col-md-4">
-                    <img class="img-responsive" alt="skeleton" src="http://206.189.45.97/~mesh20/wp-content/themes/batavia_site/images/coin.jpg">
-                    <h4>Lost Riches</h4>
-                    <p>During the 1972-76 excavation of <i>Batavia</i>, divers raised 137 shaped sandstone building blocks weighing a total of 37 tonnes. Like a giant jigsaw, they fitted together to form a magnificent portico façade.</p>
-                    <a class="continue" href="discover.html">Continue Reading</a>
+<?php $post_id = 133;
+$queried_post = get_post($post_id);?>
+<?php echo get_the_post_thumbnail($queried_post, 'full', array('class' => 'img-responsive')); ?>
+<h4><?php echo $queried_post->post_title; ?></h4>
+<?php echo $queried_post->post_excerpt; ?>
+<?php echo '<a class="continue" href="'.get_permalink($queried_post).'"><br>Continue Reading</a>';?>
                 </div>
             </div>
         </div>
